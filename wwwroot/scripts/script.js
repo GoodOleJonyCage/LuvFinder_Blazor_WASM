2c76bb9942fc97fa653e3d09414a0b6c2417fcdd
GoodOleJonyCage <adil_ahsan_khan@yahoo.com>
2023-10-08 17:26:49 -0400

    Involing js method using module way

--- wwwroot/scripts/script.js
+++ wwwroot/scripts/script.js
@@ -2,7 +2,7 @@
 //    alert(message);
 //}
 //scrolls a div's scrollbar to the very bottom
-function scrollToBottom(divid) {
+export function scrollToBottom(divid) {
     var objDiv = document.getElementById(divid);
     objDiv.scrollTop = objDiv.scrollHeight;
 }
