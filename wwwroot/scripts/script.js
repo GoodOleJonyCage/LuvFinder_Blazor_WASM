e82fa2984641540b7ab97f42e7e7b30124ae50fa
GoodOleJonyCage <adil_ahsan_khan@yahoo.com>
2023-10-08 17:13:58 -0400

    Added scroll to end js logic to chat element

---
+++ wwwroot/scripts/script.js
@@ -0,0 +1,10 @@
+﻿//function showAlert(message) {
+//    alert(message);
+//}
+//scrolls a div's scrollbar to the very bottom
+function scrollToBottom(divid) {
+    var objDiv = document.getElementById(divid);
+    objDiv.scrollTop = objDiv.scrollHeight;
+}
+
+
