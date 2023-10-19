d5a14469ba4c2c1f4841ba7fdefd7dfda84144a9
GoodOleJonyCage <adil_ahsan_khan@yahoo.com>
2023-10-19 15:51:08 -0400

    Removing hard coded reference in Program.cs

--- Program.cs
+++ Program.cs
@@ -28,20 +28,14 @@ builder.Services
                .AddScoped<RegisterUserStateContainer, RegisterUserStateContainer>()
                .AddScoped<ISearchStateContainer, SearchStateContainer>()
                .AddScoped<ILocalStorageService, LocalStorageService>();
- 
+
 // configure http client
-//builder.Services.AddScoped(x =>
-//{
-//    var apiUrl = new Uri(builder.Configuration["apiUrl"]);
-//    //var apiUrl =  builder.Configuration["apiUrl"] ;
-//    //var apiUrl = builder.Configuration.GetValue<string>("apiUrl") ;
-//    // use fake backend if "fakeBackend" is "true" in appsettings.json
-//    //if (builder.Configuration["fakeBackend"] == "true")
-//    //    return new HttpClient(new FakeBackendHandler()) { BaseAddress = apiUrl };
-//    //var apiUrl = new ConfigurationBuilder().AddJsonFile("/appsettings.json").Build().GetSection("AppSettings")["apiUrl"];
-//    return new HttpClient() { BaseAddress = apiUrl };
-//});
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7186/") });
+builder.Services.AddScoped(x =>
+{
+    var apiUrl = new Uri(builder.Configuration["apiUrl"].ToString());
+    return new HttpClient() { BaseAddress = apiUrl };
+});
+
 
 
 var host = builder.Build();
